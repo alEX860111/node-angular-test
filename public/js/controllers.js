9919cf6a1203ea672506fdee3ed13b2858e1a0fd
alEX860111 <alex86011119@gmail.com>
2014-07-20 21:17:42 +0200

    testing forms wip

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,3 +4,10 @@ app.controller('mycontroller', function($scope) {
 	$scope.title = 'My favorite cars';
 	$scope.cars = ['Mustang', 'Camaro', 'Challenger', 'Road Runner', 'Javelin'];
 });
+
+app.controller('formcontroller', function() {
+	this.data = {};
+	this.submitForm = function() {
+		console.dir(this.data);
+	};
+});
